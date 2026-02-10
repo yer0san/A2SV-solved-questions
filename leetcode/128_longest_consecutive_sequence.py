fb5c50bd4ed94df077a43af2231d4529791c1451
Yerosan Bekele <154458965+yer0san@users.noreply.github.com>
2026-02-10 16:57:28 +0300

    Create 128_longest_consecutive_sequence.py

---
+++ leetcode/128_longest_consecutive_sequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current = num
+                streak = 1
+
+                while current + 1 in num_set:
+                    current += 1
+                    streak += 1
+
+                longest = max(longest, streak)
+
+        return longest
