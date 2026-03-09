323edeed5ea2d989eed527b1fb1d468c393577d5
Yerosan Bekele <154458965+yer0san@users.noreply.github.com>
2026-03-09 18:54:51 +0300

    Add solution for Crawler Log Folder

---
+++ leetcode/1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack = []
+
+        for l in logs:
+            if l == '../':
+                if stack:
+                    stack.pop()
+            else:
+                if l == './':
+                    continue
+                stack.append(l)
+        print(stack)
+        return len(stack)
